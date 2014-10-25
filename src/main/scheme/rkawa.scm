c09ead69be3ccb7356616a1b59e79763de7753ec
hefeix <hefeix@vip.qq.com>
2014-10-25 09:17:49 -0700

    refine r <-> scheme conversion

--- src/main/scheme/rkawa.scm
+++ src/main/scheme/rkawa.scm
@@ -1,3 +1,9 @@
+;;----------------------------------------------------------------------
+
+(define-alias Exception java.lang.Exception)
+(define-alias IllegalArgumentException java.lang.IllegalArgumentException)
+(define-alias IllegalStateException java.lang.IllegalStateException)
+(define-alias Object java.lang.Object)
 
 (define-alias REXP org.rosuda.REngine.REXP)
 (define-alias REXPDouble org.rosuda.REngine.REXPDouble)
@@ -12,67 +18,186 @@
 (define-alias REXPNull org.rosuda.REngine.REXPNull)
 (define-alias REXPString org.rosuda.REngine.REXPString)
 (define-alias REXPSymbol org.rosuda.REngine.REXPSymbol)
-(define-alias REXPVector org.rosuda.REngine.REXPVector)
 (define-alias REXPUnknown org.rosuda.REngine.REXPUnknown)
-(define-alias RList org.rosuda.REngine.RList)
+(define-alias REXPVector org.rosuda.REngine.REXPVector)
+(define-alias REXPWrapper org.rosuda.REngine.REXPWrapper)
 (define-alias RFactor org.rosuda.REngine.RFactor)
+(define-alias RList org.rosuda.REngine.RList)
 
-;; Return a seq of attributes, or nil if the REXP has no attributes
-(define (r-attr-names rexp ::REXP) ::java.util.Vector
-  ((rexp:asList):names))
+;;----------------------------------------------------------------------
 
-;; Returns the attribute with the passed in name, or nil.
-(define (r-attr rexp ::REXP attr ::String) ::String
-  ((rexp:getAttribute attr):asString))
+(define (jarray->vector array)
+  (let* ((n array:length)
+         (v (make-vector n)))
+    (do ((i ::int 0 (+ i 1)))
+        ((>= i n) v)
+      (vector-set! v i (array i)))))
 
-;;  Returns true if the attribute with the passed in name exists, or false.
-(define (r-has-attr? rexp ::REXP attr ::String)
-  (rexp:hasAttribute attr))
+(define (r-na->scheme rexp)
+  0)
 
-(define (dataframe? rexp ::REXP)
-  ((r-attr rexp "class"):contains "data.frame"))
+(define (r-null->scheme rexp)
+  #!void)
 
-(define (from-dataframe rexp ::REXP)
-  (rexp:asStrings))
+(define (r-factor->scheme rexp ::REXP)
+  (let* ((value (->REXPFactor rexp)))
+    (jarray->vector (value:asStrings))))
 
-(define (from-r rexp ::REXP)
+(define (r-logical->scheme rexp ::REXP)
+  (let* ((value (->REXPLogical rexp))
+         (bools (value:isTRUE))
+         (length (value:length)))
+    (if (= length 1)
+        (bools 0)
+        bools)))
+
+(define (r-integer->scheme rexp ::REXP)
+  (let* ((value (->REXPInteger rexp))
+         (length (value:length)))
+    (if (= length 1)
+        (value:asInteger)
+        (jarray->vector (value:asIntegers)))))
+
+(define (r-numeric->scheme rexp ::REXP)
+  (let* ((value (->REXPDouble rexp))
+         (length (value:length)))
+    (if (= length 1)
+        (value:asDouble)
+        (jarray->vector (value:asDoubles)))))
+
+(define (r-string->scheme rexp ::REXP)
+  (let* ((value (->REXPString rexp))
+         (length (value:length)))
+    (if (= length 1)
+        (value:asString)
+        (jarray->vector (value:asStrings)))))
+
+(define (r-symbol->scheme rexp ::REXP)
+  #!null)
+
+(define (r-complex->scheme rexp ::REXP)
+  (r-numeric->scheme rexp))
+
+(define (r-vector->scheme rexp ::REXP)
+  (jarray->vector (rexp:asDoubles)))
+
+(define (r-list->scheme rexp ::REXP)
+  (let* ((n (rexp:length))
+         (rlist (rexp:asList))
+         (result (make-vector n)))
+    (do ((i ::int 0 (+ i 1)))
+        ((>= i length) (vector->list result))
+      (vector-set! result i (r->scheme (rlist:at i))))))
+
+(define (r->scheme rexp ::REXP)
   (cond
-    ;; int[]
-    ((rexp:isFactor) ((rexp:asFactor):asIntegers))
-    ;; int[]
-    ((rexp:isInteger) (rexp:asIntegers))
-    ;; String[]
-    ((rexp:isList) (rexp:asStrings))
-    ;; int[]
-    ((rexp:isLogical) (rexp:asIntegers))
-    ;; int[]
-    ((rexp:isNA) (rexp:asIntegers))
-    ;; int[]
-    ((rexp:isNull) (rexp:asIntegers))
-    ;; double[]
-    ((rexp:isNumeric) (rexp:asDoubles))
-    ;;
-    ((dataframe? rexp) (from-dataframe rexp))
-    ;;
-    (else (format #t "from-r: rexp = ~A~%" (rexp:toDebugString))
-          (rexp:asStrings))))
+    ((rexp:isNull) (r-null->scheme rexp))
 
-(define (to-r obj)
+    ((rexp:isFactor) (r-factor->scheme rexp))
+    ((rexp:isLogical) (r-logical->scheme rexp))
+    ((rexp:isInteger) (r-integer->scheme rexp))
+    ((rexp:isNumeric) (r-numeric->scheme rexp))
+    ((rexp:isString) (r-string->scheme rexp))
+    ((rexp:isSymbol) (r-symbol->scheme rexp))
+    ((rexp:isComplex) (r-complex->scheme rexp))
+
+    ((rexp:isVector) (r-vector->scheme rexp))
+    ((rexp:isList) (r-list->scheme rexp))
+
+    (else (format #t "r->scheme: rexp = ~A~%" (rexp:toDebugString))
+          (rexp:toDebugString))))
+
+;;----------------------------------------------------------------------
+
+(define (jstring->r value ::String)
+  (REXPString value))
+
+(define (jobject->r object ::Object)
+  (REXPWrapper:wrap object))
+
+(define (number->r value)
+  (REXPDouble (->double value)))
+
+(define (quantity->r value)
+  (number->r (quantity->number value)))
+
+(define (complex->r value)
+  (throw (IllegalArgumentException "complex->r no conversion rules")))
+
+(define (real->r value)
+  (number->r value))
+
+(define (rational->r value)
+  (number->r value))
+
+(define (integer->r value)
+  (REXPInteger (->integer value)))
+
+(define (symbol->r value)
+  (REXPSymbol (symbol->string value)))
+
+(define (keyword->r value)
+  (REXPSymbol (keyword->string value)))
+
+(define (vector->rexp-array value) ::REXP[]
+  (let* ((n (vector-length value))
+         (result (REXP[] length: n)))
+    (do ((i ::int 0 (+ i 1)))
+        ((>= i n) result)
+      (set! (result i) (scheme->r (vector-ref value i))))))
+
+(define (list->r value)
+  (REXPList (RList (vector->rexp-array (list->vector value)))))
+
+(define (vector->r value)
+  (REXPGenericVector (RList (vector->rexp-array value))))
+
+(define (string->r value ::string)
+  (REXPString value))
+
+(define (character->r value)
+  (integer->r (char->integer value)))
+
+(define (procedure->r value)
+  (throw (IllegalArgumentException "procedure->r no conversion rules")))
+
+(define (input-port->r value)
+  (throw (IllegalArgumentException "input-port->r no conversion rules")))
+
+(define (output-port->r value)
+  (throw (IllegalArgumentException "output-port->r no conversion rules")))
+
+(define (scheme->r value) ::REXP
   (cond
-    ((number? obj) (REXPDouble (as double obj)))
-    (else (format #t "to-r: obj = ~A~%" obj)
-          (REXPNull))))
+    ((number? value) (number->r value))
+    ((quantity? value) (quantity->r value))
+    ((complex? value) (complex->r value))
+    ((real? value) (real->r value))
+    ((rational? value) (rational->r value))
+    ((integer? value) (integer->r value))
+    ((symbol? value) (symbol->r value))
+    ((keyword? value) (keyword->r value))
+    ((list? value) (list->r value))
+    ((string? value) (string->r value))
+    ((character? value) (character->r value))
+    ((vector? value) (vector->r value))
+    ((procedure? value) (procedure->r value))
+    ((input-port? value) (input-port->r value))
+    ((output-port? value) (output-port->r value))
+    ((String? value) (jstring->r value))
+    ((Object? value) (jobject->r value))
+    (else (let ((e (format #f "scheme->r unknown conversion: value = ~A" value)))
+            (format #t "~A~%" e)
+            (throw (IllegalArgumentException e))))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;----------------------------------------------------------------------
 
-(define-alias Exception java.lang.Exception)
 (define-alias System java.lang.System)
-(define-alias IllegalStateException java.lang.IllegalStateException)
 
 (define-alias JRIEngine org.rosuda.REngine.JRI.JRIEngine)
+(define-alias REXPMismatchException org.rosuda.REngine.REXPMismatchException)
 (define-alias REngineEvalException org.rosuda.REngine.REngineEvalException)
 (define-alias REngineException org.rosuda.REngine.REngineException)
-(define-alias REXPMismatchException org.rosuda.REngine.REXPMismatchException)
 
 ;; (define-simple-class JriCallbacks (org.rosuda.JRI.RMainLoopCallbacks)
 ;;   ((rBusy e which)
@@ -97,7 +222,7 @@
 ;; Initialize the jri engine. This can only be done once in the
 ;; lifetime of the jvm-- not our rules- that's how the embedded R engine
 ;; seems to want things.
-(define (init-jri-engine #!optional (handler #f))
+(define (init-jri-engine #!optional (handler #!null))
   (when *jri-engine*
     (throw (IllegalStateException
              "JRIEngine can only be initialized once per lifetime of the JVM")))
@@ -117,15 +242,11 @@
                  (throw (IllegalStateException
                           "Unable to initialize JRIEngine"))))))
 
-;; Eval expression in the R engine. Will not catch any exceptions that
-;; happen during evaluation
-(define (r-eval-no-catch expr ::String)
-  (let ((r (get-jri-engine)))
-    (r:parseAndEval expr)))
+;;----------------------------------------------------------------------
 
 ;; Eval expression in the R engine. Just return the raw JRI/R wrapper,
 ;; don't convert to Scheme object
-(define (r-eval-raw expr ::String)
+(define (r% expr ::String)
   (let ((r (get-jri-engine)))
     (try-catch
       (r:parseAndEval expr)
@@ -136,8 +257,14 @@
 
 ;; Eval expression in the R engine. Convert the return value from
 ;; JRI/R to Scheme
-(define (r-eval expr)
-  (from-r (r-eval-raw expr)))
+(define (r expr)
+  (r->scheme (r% expr)))
+
+;; Eval expression in the R engine. Will not catch any exceptions that
+;; happen during evaluation
+(define (r-eval-no-catch expr ::String)
+  (let ((r (get-jri-engine)))
+    (r:parseAndEval expr)))
 
 ;; Eval expression in the R engine, wrapped (on the R side) in
 ;; try/catch. Will catch errors on the R side and convert to Exception
@@ -157,23 +284,31 @@
       (exn REXPMismatchException
         (format #t "Caught exception evaluating expression: ~A~%: ~A" expr exn)))))
 
+;; Evaluate forms that are string using r-eval-raw, otherwise, just eval
+;; Scheme code normally
+(defmacro with-r% (#!rest forms)
+  `(begin
+     ,@(map (lambda (form)
+              (if (string? form)
+                  `(r% ,form)
+                  form))
+            forms)))
+
+(defmacro with-r (#!rest forms)
+  `(begin
+     ,@(map (lambda (form)
+              (if (string? form)
+                  `(r ,form)
+                  form))
+            forms)))
+
 ;; Evaluate forms that are string using r-eval-no-catch, otherwise, just eval
-;; clojure code normally
+;; scheme code normally
 (defmacro with-r-eval-no-catch (#!rest forms)
   `(begin
      ,@(map (lambda (form)
               (if (string? form)
                   `(r-eval-no-catch ,form)
-                  form))
-            forms)))
-
-;; Evaluate forms that are string using r-eval-raw, otherwise, just eval
-;; Scheme code normally
-(defmacro with-r-eval-raw (#!rest forms)
-  `(begin
-     ,@(map (lambda (form)
-              (if (string? form)
-                  `(r-eval-raw ,form)
                   form))
             forms)))
 
@@ -187,16 +322,9 @@
                   form))
             forms)))
 
-(defmacro with-r-eval (#!rest forms)
-  `(begin
-     ,@(map (lambda (form)
-              (if (string? form)
-                  `(r-eval ,form)
-                  form))
-            forms)))
-
+;;----------------------------------------------------------------------
 ;; Assign r-name to val within the R engine
-(define (r-set! name ::String value ::REXP)
+(define (r/set! name ::String value ::REXP)
   (let ((r (get-jri-engine)))
     (try-catch
       (r:assign name value)
@@ -205,15 +333,23 @@
 
 ;; Retrieve the value with this name in the R engine. Do not convert
 ;; from JRI to Scheme type.
-(define (r-get-raw name ::String)
-  (r-eval-raw name))
+(define (r/get% name ::String)
+  (r% name))
 
 ;; Retrieve the value with this name in the R engine
-(define (r-get name)
-  (r-eval name))
+(define (r/get name)
+  (r name))
 
-(define (r-begin-gd)
-  (r-eval-raw "library(\"JavaGD\"); JavaGD()"))
+;;----------------------------------------------------------------------
 
-(define (r-end-gd)
-  (r-eval-raw "graphics.off()"))
+(define (r/dev.on)
+  (r &{library("JavaGD")
+       JavaGD()}))
+
+(define (r/dev.off)
+  (r &{graphics.off()}))
+
+(define (r/library library)
+  (r &{library("&[library]")}))
+
+;;----------------------------------------------------------------------
